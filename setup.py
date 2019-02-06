6de1c1c56cc0c8ee632d8485c56435bbeb2d2d89
HHHHhg <2894700792@qq.com>
2019-02-07 04:04:42 +0800

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r",encoding="utf8") as fh:
 
 setuptools.setup(
     name="cuclasses",
-    version="0.0.1",
+    version="0.0.2",
     author="HHHHhg",
     author_email="2894700792@qq.com",
     description="useful classes",
