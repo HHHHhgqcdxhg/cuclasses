5504550ede8c50b096915e115066260e64822a21
HHHHhg <2894700792@qq.com>
2019-02-10 16:52:53 +0800

    Update TimePrint.py

--- cuclasses/TimePrint.py
+++ cuclasses/TimePrint.py
@@ -52,23 +52,5 @@ print(4)
         else:
             rawPrint(*args, **kwargs)
 
-
 if __name__ == '__main__':
-    with timePrint() as print:
-        print(0)
-        # >>> [2019-02-10 16:23:35] 0
-
-        print(1, strf="[%Y-%m-%d %H:%M:%S]1")
-        # >>> [2019-02-10 16:23:35]1 1
-
-    with timePrint("[%Y-%m-%d %H:%M:%S]2") as print:
-        print(2)
-        # >>> [2019-02-10 16:23:35]2 2
-
-        print(3, strf="[%Y-%m-%d %H:%M:%S]3")
-        # >>> [2019-02-10 16:23:35]3 3
-
-    print(4)
-    # >>> 4
-
-    print(timePrint.__doc__)
+    print(1)
