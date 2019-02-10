d4f6fe9be51a7978635d06de4c1e5a7798ffa06d
HHHHhg <2894700792@qq.com>
2019-02-10 16:18:05 +0800

    Update __init__.py

--- cuclasses/__init__.py
+++ cuclasses/__init__.py
@@ -3,5 +3,6 @@ from .SingletonDecoration import singleton
 from .CallableDict import CallableDict
 from .StrKeyDict import StrKeyDict
 from .HeaderCopy import *
+from .TimePrint import timePrint
 
 name = "cuclasses"
